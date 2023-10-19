75d830729acbf8cb42cef8d51af939ab57bc9473
Alex Sierkov <alex.sierkov@gmail.com>
2023-10-19 14:39:08 +0200

    disable one unit test irrelevant for the public branch

--- lib/dt/cardano/byron.test.cpp
+++ lib/dt/cardano/byron.test.cpp
@@ -156,7 +156,7 @@ suite cardano_byron_suite = [] {
             expect(ed25519::verify(sig, vkey, msg));
         };
 
-        "boundary block hash"_test = [] {
+        /*"boundary block hash"_test = [] {
             auto genesis_hash = bytes_from_hex("5F20DF933584822601F9E3F8C024EB5EB252FE8CEFB24D1317DC3D432E940EBB");
             auto hash = bytes_from_hex("89D9B5A5B8DDC8D7E5A6795E9774D97FAF1EFEA59B2CAF7EAF9F8C5B32059DF4");
             auto chunk = file::read("./data/chunk-registry/immutable/526D236112DB8E38E66F37D330C85AFE0C268D81DF853DDDE4E88551EB9B0637.zstd");
@@ -168,6 +168,6 @@ suite cardano_byron_suite = [] {
             expect(blk->slot() == 0_ull);
             expect(blk->prev_hash() == genesis_hash);
             expect(blk->hash() == hash);
-        };
+        };*/
     };  
 };
