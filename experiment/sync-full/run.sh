b1c0429612ca004661596f5927b39cd6e494282f
Alex Sierkov <alex.sierkov@gmail.com>
2024-02-23 00:47:32 +0100

    sync-full experiment script synchronizes only till epoch 465

--- experiment/sync-full/run.sh
+++ experiment/sync-full/run.sh
@@ -10,7 +10,7 @@ for iter in `seq 1 3`; do
         echo "kernel caches flushed, starting monitoring and logging processes"
         bash -c "while true; do docker stats --no-stream >> /data/cardano/dt-sync-http-docker-stats-$iter-$threads.log; sleep 1; done" &
         echo "starting the benchmark"
-        docker run --cpus $threads --rm -v /data/cardano:/data/cardano dt /bin/bash -c "sudo chown -R dev:dev /data; DT_LOG=/data/cardano/dt-sync-http-$iter-$threads.log DT_WORKERS=$threads ./dt sync-http /data/cardano"
+        docker run --cpus $threads --rm -v /data/cardano:/data/cardano dt /bin/bash -c "sudo chown -R dev:dev /data; DT_LOG=/data/cardano/dt-sync-http-$iter-$threads.log DT_WORKERS=$threads ./dt sync-http /data/cardano --max-epoch=465"
         echo "stopping monitoring and logging processes"
         jobs -p | xargs -r kill
     done
